89c8620b3c8e8468d5d67c0a7278361ebaf58cf5
Kasia Piekarz <katjerinka@gmail.com>
2018-09-12 15:13:40 +0200

    adding function random

---
+++ app.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const bodyBg = document.querySelector("body");
+
+    function randomColor() {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        const bg = "background-color:rgb(" + r + ", " + g + ", " + b + ");";
+        console.log("background color:" + bg);
+        bodyBg.style = bg;
+    }
+    setInterval(randomColor, 5000);
+  
+});
