8a3de18264dc542a5ad9f52148ad79b78371fb34
Kasia Piekarz <katjerinka@gmail.com>
2018-09-12 16:43:19 +0200

    adding window event

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const bodyBg = document.querySelector("body");
+const bodyBg = document.querySelector("body");
 
-    function randomColor() {
-        const r = Math.floor(Math.random() * 256);
-        const g = Math.floor(Math.random() * 256);
-        const b = Math.floor(Math.random() * 256);
-        const bg = "background-color:rgb(" + r + ", " + g + ", " + b + ");";
-        console.log("background color:" + bg);
-        bodyBg.style = bg;
-    }
-    setInterval(randomColor, 3000);
-  
-});
+function randomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    const bg = "background-color:rgb(" + r + ", " + g + ", " + b + ");";
+    console.log("background color:" + bg);
+    bodyBg.style = bg;
+}
+setInterval(randomColor, 3000);
+window.onload = function () {
+    randomColor();
+}
